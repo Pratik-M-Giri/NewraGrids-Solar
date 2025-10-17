a53d3b653216a96c386480f0eba2f463b4996ddd
vishal <vp461365@gmail.com>
2025-10-17 23:48:54 +0530

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./Redux/Store";
+import { store } from "./Redux/store";
 import App from "./App";
 import './index.css'
 ReactDOM.createRoot(document.getElementById("root")).render(
